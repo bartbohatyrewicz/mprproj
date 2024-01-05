a36a53293591c3c9d073a89492765b5ad2d06eb6
bartbohatyrewicz <s26860@pjwstk.edu.pl>
2024-01-05 18:37:44 +0100

    init

---
+++ src/main/java/com/mpr/proj/MprprojApplication.java
@@ -0,0 +1,13 @@
+package com.mpr.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MprprojApplication.class, args);
+	}
+
+}
